96eb4b1f7942aa232effda49f0f4fb3f9b14d582
ginobrancazio <ginobrancazio@hotmail.com>
2026-02-24 23:14:50 +0000

    Update admin.js

--- acmh/admin.js
+++ acmh/admin.js
@@ -24,6 +24,7 @@ function setupAuthObserver() {
       currentUser = user;
       showAdminDashboard();
       loadGameInfo();
+      processRecurringCosts();
     } else {
       currentUser = null;
       showLoginScreen();
